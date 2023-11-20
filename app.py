02c962c70e8997109ae7ce616207ae09409c51f7
BarJucha <bjucha@studnet.agh.edu.pl>
2023-11-20 17:58:47 +0100

    Podstawowe działanie currentWeather

--- app.py
+++ app.py
@@ -4,19 +4,46 @@ import hashlib
 import os
 import login
 import currentWeather
+import requests
 app = Flask(__name__, static_url_path='/static')
 
 app.secret_key = 'klucz_sesji'
 
 @app.route('/')
 def index():
-    if request.method == 'POST':
-        #Odbierz dane od front-endu (miasto)
-    
-        city = request.get_json()['city']
-        currentWeather.getCurrentWeather(city)
+    return render_template('main.html')
+
+@app.route('/currentWeather/<string:city>')
+def currentWeather(city):
+    # Klucz API dostarczony przez WeatherAPI
+    api_key = 'c71bd51c9e09474a8db153108231911'
+
+    # Wysyłanie zapytania do WeatherAPI
+    weather_api_url = f'http://api.weatherapi.com/v1/current.json?key={api_key}&q={city}&aqi=no'
+    response = requests.get(weather_api_url)
+
+    # Sprawdź, czy zapytanie było udane
+    if response.status_code == 200:
+        weather_data = response.json()
+
+        # Przetwórz dane pogodowe
+        temperature = weather_data['current']['temp_c']
+        condition = weather_data['current']['condition']['text']
+        icon = weather_data['current']['condition']['icon']
+        city = weather_data['location']['name']
+        data = weather_data['location']['localtime']
+
+        # Przygotuj dane do przekazania do szablonu HTML
+
+        return render_template('main.html', temp = temperature, warunki = condition, miasto = city, data = data, icon = icon)
     else:
-        return render_template('main.html')
+        return redirect(url_for('index'))
+
+@app.route('/submitCity', methods=['POST','GET'])
+def submitWeather():
+    if request.method=='POST':
+        miasto = str(request.form['city'])
+    return redirect(url_for('currentWeather',city=miasto))
 
 if __name__ == '__main__':
     app.run(debug=True)
